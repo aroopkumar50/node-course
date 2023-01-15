2aeecd58573c25e7c21f30e0ad45e961654f4e05
Aroop Kumar <aroopkumar50@gmail.com>
2023-01-15 19:01:53 +0530

    Add more tests

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -106,7 +106,7 @@ const upload = multer({
 });
 
 router.post(
-    "/users/me/avatars",
+    "/users/me/avatar",
     auth,
     upload.single("avatar"),
     async (req, res) => {
@@ -123,13 +123,13 @@ router.post(
     }
 );
 
-router.delete("/users/me/avatars", auth, async (req, res) => {
+router.delete("/users/me/avatar", auth, async (req, res) => {
     req.user.avatar = undefined;
     await req.user.save();
     res.send();
 });
 
-router.get("/users/:id/avatars", async (req, res) => {
+router.get("/users/:id/avatar", async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
 
